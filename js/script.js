52e40928f2073593625636aea4d9d4e1696649ee
oaguillondev <aguillon_@hotmail.com>
2014-02-18 18:13:29 -0500

    add video channel in canal.html

--- js/script.js
+++ js/script.js
@@ -2,9 +2,11 @@
 function sizeScreen(){
 	var alto = $(window).height();
 	var ancho = $(window).width();
+	var anchoContainer = $("#container").width();
 
 	$("section#debug").html("Ancho: "+ancho+" Alto:"+alto);	
-	$("#container").css({"height":alto+"px"});	
+	$("#container").css({"height":alto+"px"});
+	$("body.noticias #container iframe").css({"width":anchoContainer-10+"px","height":alto-150+"px"})
 }
 
 $(window).resize(function(){ sizeScreen(); });
