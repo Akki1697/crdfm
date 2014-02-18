20758a42552f55aa15df62a10e8a5feffaa71c71
oaguillondev <aguillon_@hotmail.com>
2014-02-18 09:45:03 -0500

    add files

---
+++ images/script.js
@@ -0,0 +1,23 @@
+
+function sizeScreen(){
+	alto = $(window).height();
+	ancho = $(window).width();
+
+	$("section#debug").html("Ancho: "+ancho+" Alto:"+alto);
+	
+	$("#container").css({"height":alto+"px"});
+}
+
+$(window).resize(function(){
+	sizeScreen();
+});
+	
+
+
+$(document).on("ready",function(){
+
+	
+
+sizeScreen();
+
+});
